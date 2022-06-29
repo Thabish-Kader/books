728734f5721042664f4dca3350e78fe90807faf6
Thabish <kadertabish2@gmail.com>
2022-06-29 12:43:27 +0400

    'ch9'

--- config/settings.py
+++ config/settings.py
@@ -63,7 +63,7 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend', 
 )
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 ACCOUNT_SESSION_REMEMBER = True
 ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
 ACCOUNT_USERNAME_REQUIRED = False 
@@ -112,6 +112,7 @@ DATABASES = {
     default="postgres://postgres@db/postgres")
     }
 
+DEFAULT_FROM_EMAIL = 'admin@djangobookstore.com'
 
 
 # Password validation
